0d63688262f0471465648de4f4e339c86fac9cd2
Aleksey <meysonsib200189@mail.ru>
2023-05-19 10:44:26 +0700

    закончил добавление и инициализацию RecyclerView и инициализирова его в коде с добавлением пробного списка из двух городов

--- app/src/main/java/com/bignerdranch/android/weatherappcurse/fragments/HoursFragment.kt
+++ app/src/main/java/com/bignerdranch/android/weatherappcurse/fragments/HoursFragment.kt
@@ -1,19 +1,39 @@
 package com.bignerdranch.android.weatherappcurse.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.bignerdranch.android.weatherappcurse.R
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bignerdranch.android.weatherappcurse.adapter.WeatherAdapter
+import com.bignerdranch.android.weatherappcurse.databinding.FragmentHourseBinding
+import com.bignerdranch.android.weatherappcurse.model.WeatherModel
 
 class HoursFragment : Fragment() {
+
+    private lateinit var binding: FragmentHourseBinding
+    private lateinit var adapter: WeatherAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_hourse, container, false)
+    ): View {
+        binding = FragmentHourseBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() = with(binding) {
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+        adapter = WeatherAdapter()
+        recyclerView.adapter = adapter
+        val list = listOf(WeatherModel("Новосибирск", "22:00", "sunny", "45", "", "", "", ""),WeatherModel("Омск", "12:00", "sunny", "25", "", "", "", ""))
+        adapter.submitList(list)
     }
 
     companion object {
